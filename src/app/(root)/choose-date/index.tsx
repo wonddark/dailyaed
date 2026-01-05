042281e67039dc53e56e86415916e49f16d25cc5
Osmanys Fuentes L <ossmanys@gmail.com>
2026-01-05 15:59:54 +0400

    Add `ChooseDate` screen with single and range date selection
    - Implement `ChooseDate` screen allowing both single-day and range selection modes.
    - Integrated `react-native-ui-datepicker` with dynamic styling and localization support.
    - Added navigation to Daily and Monthly Summary screens based on selected date(s).
    - Applied themed and responsive styles in line with app design.

---
+++ src/app/(root)/choose-date/index.tsx
@@ -0,0 +1,206 @@
+import React, { useEffect, useState } from "react";
+import { StyleSheet } from "react-native-unistyles";
+import { RootView, View } from "@/components/views";
+import { Platform } from "react-native";
+import { Link, useLocalSearchParams, useRouter } from "expo-router";
+import Button from "@/components/button";
+import DateTimePicker, { DateType } from "react-native-ui-datepicker";
+import { useTranslation } from "react-i18next";
+import dayjs, { Dayjs } from "dayjs";
+import PageHeader from "@/components/PageHeader";
+import SimpleLineIcons from "@expo/vector-icons/SimpleLineIcons";
+
+const ChooseDate = () => {
+  const { t, i18n } = useTranslation();
+  const { dismiss } = useRouter();
+  const { pickDay } = useLocalSearchParams();
+  const [selected, setSelected] = useState<Dayjs>(dayjs());
+
+  const onChangeDate = (params: { date: DateType }) => {
+    setSelected(dayjs(params.date));
+  };
+
+  const onMonthChange = (month: number) => {
+    setSelected(dayjs(selected).month(month));
+  };
+
+  const onYearChange = (month: number) => {
+    setSelected(dayjs(selected).year(month));
+  };
+
+  useEffect(() => {
+    if (Platform.OS === "web" && document !== undefined) {
+      document.title = "Choose a date - DailyAED";
+    }
+  }, []);
+
+  return (
+    <RootView>
+      <PageHeader
+        title={t("chooseDate")}
+        subtitle={t("chooseDateSubtitle")}
+        sideAction={false}
+      />
+
+      {pickDay ? (
+        <DateTimePicker
+          mode="single"
+          date={selected}
+          onChange={onChangeDate}
+          onMonthChange={onMonthChange}
+          onYearChange={onYearChange}
+          styles={styles}
+          style={styles.calendar}
+          locale={i18n.resolvedLanguage}
+          disabledDates={(date) => dayjs(date).isAfter(dayjs(), "day")}
+          components={{
+            IconNext: (
+              <SimpleLineIcons
+                name="arrow-right"
+                size={14}
+                color={styles.button_next_image.color}
+              />
+            ),
+            IconPrev: (
+              <SimpleLineIcons
+                name="arrow-left"
+                size={12}
+                color={styles.button_prev_image.color}
+              />
+            ),
+          }}
+        />
+      ) : (
+        <DateTimePicker
+          mode="range"
+          startDate={selected.date(1)}
+          endDate={selected.date(selected.daysInMonth())}
+          onMonthChange={onMonthChange}
+          onYearChange={onYearChange}
+          styles={styles}
+          style={styles.calendar}
+          locale={i18n.resolvedLanguage}
+          disabledDates={(date) => dayjs(date).isAfter(dayjs(), "day")}
+          initialView={pickDay ? "day" : "month"}
+          components={{
+            IconNext: (
+              <SimpleLineIcons
+                name="arrow-right"
+                size={14}
+                color={styles.button_next_image.color}
+              />
+            ),
+            IconPrev: (
+              <SimpleLineIcons
+                name="arrow-left"
+                size={12}
+                color={styles.button_prev_image.color}
+              />
+            ),
+          }}
+        />
+      )}
+
+      <View style={styles.buttonsContainer}>
+        <Link
+          href={
+            pickDay
+              ? `/daily-summary?date=${selected.format("YYYY-MM-DD")}`
+              : `/monthly-summary?date=${selected.format("YYYY-MM")}`
+          }
+          asChild
+          dismissTo
+        >
+          <Button label={t("apply")} />
+        </Link>
+        <Button onPress={() => dismiss()} label={t("cancel")} variant="ghost" />
+      </View>
+    </RootView>
+  );
+};
+
+export default ChooseDate;
+
+const styles = StyleSheet.create((theme) => ({
+  title: {
+    fontSize: 24,
+    fontWeight: 300,
+    marginBottom: 32,
+  },
+  calendar: {
+    marginBottom: 24,
+  },
+  day_cell: {
+    padding: 2,
+  },
+  day_label: {
+    color: theme.colors.body,
+  },
+  today: {
+    borderWidth: 1,
+    borderColor: `${theme.colors.primary}37`,
+    borderRadius: 8,
+  },
+  today_label: {
+    color: theme.colors.primary,
+  },
+  month_label: {
+    color: theme.colors.body,
+    textTransform: "uppercase",
+  },
+  year_label: {
+    color: theme.colors.body,
+  },
+  month_selector_label: {
+    color: theme.colors.body,
+    fontWeight: 500,
+  },
+  year_selector_label: {
+    color: theme.colors.body,
+    fontWeight: 500,
+  },
+  selected_label: {
+    color: theme.colors.primaryContrast,
+    fontWeight: 500,
+  },
+  selected: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+  },
+  range_middle: {
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+  },
+  range_middle_label: {
+    color: theme.colors.primaryContrast,
+  },
+  range_start_label: {
+    color: theme.colors.primaryContrast,
+  },
+  range_end_label: {
+    color: theme.colors.primaryContrast,
+  },
+  weekdays: {
+    paddingVertical: 2,
+    marginTop: 4,
+    marginBottom: 16,
+  },
+  weekday_label: {
+    fontWeight: 500,
+    color: theme.colors.body,
+  },
+  disabled_label: {
+    color: theme.colors.muted,
+  },
+  button_prev_image: {
+    color: theme.colors.body,
+    backgroundColor: theme.colors.body,
+  },
+  button_next_image: {
+    color: theme.colors.body,
+  },
+  buttonsContainer: {
+    gap: 8,
+    width: "100%",
+  },
+}));
